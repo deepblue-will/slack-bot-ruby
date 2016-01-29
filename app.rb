91cdef7c1f6c2695d76c438111c7d7c196b80b66
deepblue_will <deepblue.will@gmail.com>
2016-01-30 01:27:14 +0900

    post random message

--- app.rb
+++ app.rb
@@ -2,13 +2,14 @@ require 'sinatra'
 require 'json'
 
 post '/gateway' do
-  logger.info params
-  logger.info ENV["TOKEN"]
-  logger.info ENV["REPLY_WORDS"]
-  logger.info ENV["CHANNELS"]
+  return if params[:token] != ENV["TOKEN"]
+  ENV["CHANNELS"].split(",").include?(params[:channel_name])
+  
+  reply_words = ENV["REPLY_WORDS"].split(",")
+  respond_message(reply_words[rand(reply_words.size - 1)])
 end
 
-def respond_message message
+def respond_message(message)
   content_type :json
   {text: message}.to_json
 end
